d633489f8323f557bb43b3c7fe7b6e445abf38bc
LOVERANDROID <1099495519@qq.com>
2018-12-18 11:16:49 +0800

    Update ArraySum.java

--- LintCode/src/com/lintcode/ArraySum.java
+++ LintCode/src/com/lintcode/ArraySum.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class ArraySum {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		int sum = 0;
 		Scanner scanner1 = new Scanner(System.in);
 		int n = scanner1.nextInt();
